d3ea888ac162c7d36a95d8a2b62a5e6e8feac289
KingTigrou <matth76330@live.fr>
2022-02-15 15:46:58 +0100

    update pin

--- setinp.py
+++ setinp.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO         # import RPi.GPIO module
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)          # choose BCM or BOARD
-GPIO.setup(26, GPIO.OUT)        # set a port/pin as an output
-GPIO.output(26, 1)              # set port/pin value to 0/GPIO.LOW/False
+GPIO.setup(17, GPIO.OUT)        # set a port/pin as an output
+GPIO.output(17, 1)              # set port/pin value to 0/GPIO.LOW/False
